73f047e2590d7a4b98b501e368a7c8efbcab32e2
KaramiHossein <hkp6785@gmail.com>
2016-11-15 15:30:41 +0330

    اصلاح انتخاب روزها براي يک ماه پس و پيش

--- NRI_STLF_MATLABComponentProject/M_Files/LoadTraining_online.m
+++ NRI_STLF_MATLABComponentProject/M_Files/LoadTraining_online.m
@@ -10,7 +10,7 @@ if ((mm==1)||(mm==7))
 elseif ((mm==12)||(mm==6))
     IND1=[find((A(:,1)==yy).*(A(:,2)==mm-1));find((A(:,1)<yy).*(A(:,2)==mm-1));find((A(:,1)<yy).*(A(:,2)==mm))];
 else
-    IND1=[find((A(:,1)==yy).*(A(:,2)==mm-1));find((A(:,1)<yy).*(A(:,2)==mm+1));find((A(:,1)<yy).*(A(:,2)==mm));find((A(:,1)<yy).*(A(:,2)==mm-1))];
+    IND1=[find((A(:,1)==yy).*(A(:,2)==mm).*(A(:,3)<dd));find((A(:,1)==yy).*(A(:,2)==mm-1));find((A(:,1)<yy).*(A(:,2)==mm+1));find((A(:,1)<yy).*(A(:,2)==mm));find((A(:,1)<yy).*(A(:,2)==mm-1))];
 end
 
 
